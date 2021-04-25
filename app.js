70fb413cac7fdf92c2b5fc39a7f2af3c0a96e48f
IbrahemSarayrah <ibrahem.sarayrah@gmail.com>
2021-04-25 13:48:19 +0300

    lab

---
+++ app.js
@@ -0,0 +1,23 @@
+let userName = prompt(`What is Your Name ?`);
+console.log(`The username is ` + userName);
+
+let userAge = prompt(`What is your age ?`);
+console.log(`The User Age is ` + userAge);
+
+let userLocation = prompt(`What is your location ?`);
+console.log(`The User Location is ` + userLocation);
+
+let favPlace = prompt(`What is Your Fav-Place to visit ?`);
+console.log(`The user Fav-Place is ` + favPlace);
+
+alert(
+  `Welcome ` +
+    `Your name is ` +
+    userName +
+    ` and Your Age is ` +
+    userAge +
+    ` and the current location for you is ` +
+    userLocation +
+    ` and your fav-place to visit is ` +
+    favPlace
+);
